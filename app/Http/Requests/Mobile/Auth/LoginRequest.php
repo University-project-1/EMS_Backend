e2b9da9614d733b0209ee18578ffa7f6fd6c0bca
wasem <wasemalhariri13@gmail.com>
2026-06-16 19:02:42 +0300

    fix bug

--- app/Http/Requests/Mobile/Auth/LoginRequest.php
+++ app/Http/Requests/Mobile/Auth/LoginRequest.php
@@ -24,7 +24,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'phone' => ['required', 'string', , new Phone()],
+            'phone' => ['required', 'string',new Phone()],
             'password' => ['required', 'string'],
         ];
     }
