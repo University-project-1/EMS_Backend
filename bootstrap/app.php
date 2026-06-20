e8b3dbfc3ee28ea565a0dae18a01fa6264d442a9
wasem <wasemalhariri13@gmail.com>
2026-06-20 11:49:53 +0300

    add EnsureUserIsAdmin middleware to bootstrap/app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\ApiLocalization;
+use App\Http\Middleware\EnsureUserIsAdmin;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -22,6 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias(['type.admin' => EnsureUserIsAdmin::class]);
         $middleware->api([
             'ApiLocalization' => ApiLocalization::class,
         ]);
