082e97511dea748f2cc455943c6dafc120aebf64
MHD-fawzy-sukkar <eng.fawzysukkar@gmail.com>
2026-06-28 18:29:18 +0300

    add invitation policies

--- app/Http/Requests/SystemUser/Exhibitor/RegisterExhibitorRequest.php
+++ app/Http/Requests/SystemUser/Exhibitor/RegisterExhibitorRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests\SystemUser\Exhibitor;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -18,8 +17,6 @@ class RegisterExhibitorRequest extends FormRequest
 
     /**
      * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
