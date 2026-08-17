d0653efc23841d2fa5c2291a27ad070eeb628f57
wasem <wasemalhariri13@gmail.com>
2026-08-17 18:54:53 +0300

    harden-api-rate-limits

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-Route::prefix('v1')->group(function () {
-    require __DIR__ . '/api/v1/admin.php';
-    require __DIR__ . '/api/v1/exhibitor.php';
-    require __DIR__ . '/api/v1/mobile.php';
+Route::prefix('v1')->middleware('throttle:api')->group(function () {
+    require __DIR__.'/api/v1/admin.php';
+    require __DIR__.'/api/v1/exhibitor.php';
+    require __DIR__.'/api/v1/mobile.php';
 });
