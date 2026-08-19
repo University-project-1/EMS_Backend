34001f0f03e3185082b20a28b2dd188babe0ee5e
MHD-fawzy-sukkar <eng.fawzysukkar@gmail.com>
2026-08-19 12:05:25 +0300

    fix test errors

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,9 +30,9 @@ final class DatabaseSeeder extends Seeder
                 EventSeeder::class,
                 LeadSeeder::class,
                 SavedSeeder::class,
-                NotificationSeeder::class,
                 ReportSeeder::class,
                 ReviewSeeder::class,
+                NotificationSeeder::class,
             ]);
         }
 
